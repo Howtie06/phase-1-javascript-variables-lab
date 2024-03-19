c3f583798e497c05ef888e7b9000f685f21d1e03
audrey cherop <audreycherop06@gmail.com>
2024-03-19 13:46:40 +0300

    variables

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+// Declare and initialize companyName as a const variable
+const companyName = 'Scuber';
+
+// Declare and initialize mostProfitableNeighborhood as a let variable
+let mostProfitableNeighborhood = 'Chelsea';
+
+// Declare and initialize companyCeo as a let variable
+let companyCeo = 'Susan Smith';
+
